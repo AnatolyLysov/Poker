3702e04432ab1c3ba8c333c93d6a2bb49949dd2f
Anatoly <56615869+AnatolyLysov@users.noreply.github.com>
2025-05-19 15:47:31 +0000

    Changes to be committed: 	new file:   game_L12 	modified:   game_L12.c

--- my/C_programming/lessons/lesson12/game_L12.c
+++ my/C_programming/lessons/lesson12/game_L12.c
@@ -2,7 +2,23 @@
 
 int main(void)
     {
+        // This program creates a 10x20 array of characters and fills it with a border of '#' characters.   
+        char mas[10][21];
+        int x = 10, y = 5;
+         
+        sprintf(mas[0], "####################");
+
+        for (int i = 1; i < 9; i++) {
+            sprintf(mas[i], "#                  #");
+        }
+
+        sprintf(mas[9], "####################");
     
+        mas[y][x] = '@';
     
+        // Print the array
+        for (int i = 0; i < 10; i++) {
+            printf("%s\n", mas[i]);
+        }
         return 0;
     }

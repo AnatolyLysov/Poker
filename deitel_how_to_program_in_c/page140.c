66f4a7106dd72accaafb272ac1bab487e3904c88
AnatolyLysov <anatoly.lysov@gmail.com>
2025-06-02 18:59:55 +0300

    Changes to be committed: 	new file:   ../.vscode/c_cpp_properties.json 	new file:   ../.vscode/settings.json 	new file:   page140 new file:   page140.c 	modified:   ../my/info/diary

---
+++ deitel_how_to_program_in_c/page140.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int count = 10;
+    
+    // Counter controlled repetition: loop from 1 to 10
+    for (int i = 1; i <= count; i++) {
+        printf("Counter: %d\n", i);
+    }
+    
+    return 0;
+}
